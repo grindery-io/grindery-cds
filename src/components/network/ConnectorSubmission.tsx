21e68f3d12309e095fab7226b45b3e2a469072a9
Egor Miliukov <milukove@gmail.com>
2023-06-30 23:26:39 +0300

    set batchSizeOpenAI from abi to cds convert requests

--- src/components/network/ConnectorSubmission.tsx
+++ src/components/network/ConnectorSubmission.tsx
@@ -267,6 +267,7 @@ const ConnectorSubmission = (props: Props) => {
           name: state.form.entry.name,
           icon: state.form.entry.icon,
           description: state.form.entry.description,
+          batchSizeOpenAI: 20,
         },
         {
           headers: {
